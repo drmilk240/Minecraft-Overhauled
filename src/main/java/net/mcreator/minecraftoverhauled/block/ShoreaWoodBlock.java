b6fcfbabb5e4b167f1ee912b53e7a38a1d2f39cd
redstonerblast <conno@LAPTOP-EDT9LP9K>
2021-04-23 18:09:19 -0500

    IssueFix

--- src/main/java/net/mcreator/minecraftoverhauled/block/ShoreaWoodBlock.java
+++ src/main/java/net/mcreator/minecraftoverhauled/block/ShoreaWoodBlock.java
@@ -1,17 +1,34 @@
 
 package net.mcreator.minecraftoverhauled.block;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.common.IPlantable;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.minecraftoverhauled.itemgroup.SavannaItemGroup;
+import net.mcreator.minecraftoverhauled.MinecraftOverhauledModElements;
+
+import java.util.List;
+import java.util.Collections;
 
 @MinecraftOverhauledModElements.ModElement.Tag
 public class ShoreaWoodBlock extends MinecraftOverhauledModElements.ModElement {
-
 	@ObjectHolder("minecraft_overhauled:shorea_wood")
 	public static final Block block = null;
-
 	public ShoreaWoodBlock(MinecraftOverhauledModElements instance) {
 		super(instance, 46);
-
 	}
 
 	@Override
@@ -19,15 +36,10 @@ public class ShoreaWoodBlock extends MinecraftOverhauledModElements.ModElement {
 		elements.blocks.add(() -> new CustomBlock());
 		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(SavannaItemGroup.tab)).setRegistryName(block.getRegistryName()));
 	}
-
 	public static class CustomBlock extends Block {
-
 		public CustomBlock() {
-			super(
-
-					Block.Properties.create(Material.WOOD).sound(SoundType.WOOD).hardnessAndResistance(2f, 2f).lightValue(0).harvestLevel(0)
-							.harvestTool(ToolType.AXE));
-
+			super(Block.Properties.create(Material.WOOD).sound(SoundType.WOOD).hardnessAndResistance(2f, 2f).lightValue(0).harvestLevel(0)
+					.harvestTool(ToolType.AXE));
 			setRegistryName("shorea_wood");
 		}
 
@@ -43,13 +55,10 @@ public class ShoreaWoodBlock extends MinecraftOverhauledModElements.ModElement {
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(this, 1));
 		}
-
 	}
-
 }
